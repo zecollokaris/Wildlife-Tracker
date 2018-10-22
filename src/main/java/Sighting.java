1f3d983999ebb8985db5a0db3a15ffdc47bdfac2
zecollokaris <collo.kariss@gmail.com>
2018-10-22 15:37:30 +0300

    setting getters.

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -10,6 +10,16 @@ public class Sighting {
     private String month;
 
 
+    private DB db=new DB();
+    public Sighting (double id,String ranger_name,String location,String doing,String animal,String date,String month) {
+        this.ranger_name=ranger_name;
+        this.location=location;
+        this.doing=doing;
+        this.animal=animal;
+        this.date=date;
+        this.month=month;
+    }
+    
 
 
 }
