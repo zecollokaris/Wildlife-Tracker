6f4a325f8f5ea3977019d11435682b674013aab2
zecollokaris <collo.kariss@gmail.com>
2018-10-22 17:18:23 +0300

    added route for sighting

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,11 +7,35 @@ import static spark.Spark.*;
 
 
 //APP MAIN CLASS METHOD
-
-
 public class App{
-
+    static DB db=new DB();
+    static Map<String,Object> model=new HashMap<String,Object>();
     public static void main(String[] args) {
+//      STATIC FILES LOCATION!!
+        staticFileLocation("/public");
+//      MAIN VTL FILE!!
+        String layout = "templates/layout.vtl";
+
+
+//\\!!_  THE APPLICATION ROUTES ARE LOCATED HERE!!  _!!//\\
+        
+
+//      Route for viewing Sightings!
+    get("/sightings",(request, response) -> {
+
+        model.put("sighting",db.getCon().createQuery("SELECT * FROM sighting;").executeAndFetch(Sighting.class));
+        model.put("template","templates/sightings.vtl");
+        return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+
+
+
+
+
+
+
+
+
 
     }
 
