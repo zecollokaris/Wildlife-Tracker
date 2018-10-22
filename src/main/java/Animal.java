4ee4e2ef7388117392275bb6f7bdf713bd010356
zecollokaris <collo.kariss@gmail.com>
2018-10-22 19:36:19 +0300

    added pvivate classes in animal.java

---
+++ src/main/java/Animal.java
@@ -0,0 +1,16 @@
+import org.sql2o.Connection;
+public class Animal{
+//  THIS WILL ONLY BE ACCESSED WITHIN THI CLASS!!
+    private double id;
+    private Sighting name;
+    private Sighting category;
+    private Sighting health;
+    private Sighting age;
+    private Sighting status;
+    private DB db=new DB();
+    public Stylist(double id,String name,String category,String health, String age, String status){
+
+
+
+
+}
