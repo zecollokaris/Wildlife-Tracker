95caa83ca31d8e45870adc0ac504bb0795411aea
zecollokaris <collo.kariss@gmail.com>
2018-10-22 15:36:06 +0300

    added private

---
+++ src/main/java/Sighting.java
@@ -0,0 +1,15 @@
+import org.sql2o.Connection;
+public class Sighting {
+//  THIS WILL ONLY BE ACCESSED WITHIN THI CLASS!!
+    private double id;
+    private String ranger_name;
+    private String location;
+    private String doing;
+    private String animal;
+    private String date;
+    private String month;
+
+
+
+
+}
