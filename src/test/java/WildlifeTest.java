ad9d4fc5a962d248ddda63bf1178a7239a899b86
zecollokaris <collo.kariss@gmail.com>
2018-10-22 14:21:21 +0300

    done with build test moving to assertions

--- src/test/java/WildlifeTest.java
+++ src/test/java/WildlifeTest.java
@@ -13,6 +13,7 @@ public class WildlifeTest{
     public DataBaseRule db=new DataBaseRule();
 
 
+//  TEST TO INSERT IN TABLE SIGHTING IN DTABASE!!
     public boolean InsertSighting() {
         db.getCon().createQuery("INSERT INTO sighting (ranger_name,location,doing,animal,date,month) VALUES (:id,:ranger_name,:location,:doing,:animal,:date,:month);")
         .addParameter("ranger_name","Collins Kariuki")
@@ -25,16 +26,19 @@ public class WildlifeTest{
         return true;
     }
 
+//  TEST FOR INSERT IN TABLE ANIMAL IN DATABASE!!
     public boolean InsertAnimal() {
         db.getCon().createQuery("INSERT INTO animal (id,name,category,health,age,status) VALUES (:id,:name,:category,:health,:age,:status);")
-        .addParameter("ranger_name","Collins Kariuki")
-        .addParameter("location","Near The Lake")
-        .addParameter("doing","Feeding on grass")
-        .addParameter("animal","zebra")
-        .addParameter("date","21st")
-        .addParameter("month","September")
+        .addParameter("id","12345")
+        .addParameter("name","Zebra")
+        .addParameter("category","Land Vertebrate")
+        .addParameter("health","healthy")
+        .addParameter("age","newborn")
+        .addParameter("status","endangered")
         .executeUpdate();
+        return true;
     }
+
 
 
 }
