5f30a76006adf48f1aed845629b46d2edddc4ad9
zecollokaris <collo.kariss@gmail.com>
2018-10-22 12:33:54 +0300

    added override rule

--- src/test/java/DataBaseRule.java
+++ src/test/java/DataBaseRule.java
@@ -9,6 +9,11 @@ public class DataBaseRule extends ExternalResource{
     private Connection con;
     private Sql2o sql2o=new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test","karis","Kar!s123");
 
+//  @OVERRIDE
+    @Override
+    public void before(){
+        con=sql2o.open();
+    }
 
 
 
