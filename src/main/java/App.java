0fdfacbd4ca47ebd1e96b44dcdd3836ccffd0e6a
zecollokaris <collo.kariss@gmail.com>
2018-10-22 11:52:56 +0300

    added imports for app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,14 @@
+//IMPORTS!
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+
+//APP MAIN CLASS METHOD
+
+
 public class App{
 
     public static void main(String[] args) {
