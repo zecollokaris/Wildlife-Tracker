2eaca3d0900ac4e7ff4b3cc9ec4f8883570d66ba
zecollokaris <collo.kariss@gmail.com>
2018-10-22 14:05:59 +0300

    added insert sightings test

--- src/test/java/WildlifeTest.java
+++ src/test/java/WildlifeTest.java
@@ -4,13 +4,27 @@
 //IMPORTS!
 import org.junit.Test;
 import org.junit.Rule;
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class WildlifeTest{
-    @Rule
-    public DataBaseRule db= DataBaseRule();
 
-    public boolean InsertSighting(){
-        db.getCon().createQuery("INSERT INTO sighting (ranger_name,location)")
+//  DATABASE RULE!!
+    @Rule
+    public DataBaseRule db=new DataBaseRule();
+
+
+    public boolean InsertSighting() {
+        db.getCon().createQuery("INSERT INTO sighting (ranger_name,location,doing,animal,date,month) VALUES (:id,:ranger_name,:location,:doing,:animal,:date,:month);")
+        .addParameter("ranger_name","Collins Kariuki")
+        .addParameter("location","Near The Lake")
+        .addParameter("doing","Feeding on grass")
+        .addParameter("animal","zebra")
+        .addParameter("date","21st")
+        .addParameter("month","September")
+        .executeUpdate();
+        return true;
     }
+
+
+
 }
