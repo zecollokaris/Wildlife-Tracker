9f29c19b351873015bfa0bb268df07eaf8e64c75
zecollokaris <collo.kariss@gmail.com>
2018-10-22 14:06:44 +0300

    added insert animal test

--- src/test/java/WildlifeTest.java
+++ src/test/java/WildlifeTest.java
@@ -25,6 +25,16 @@ public class WildlifeTest{
         return true;
     }
 
+    public boolean InsertAnimal() {
+        db.getCon().createQuery("INSERT INTO animal (id,name,category,health,age,status) VALUES (:id,:name,:category,:health,:age,:status);")
+        .addParameter("ranger_name","Collins Kariuki")
+        .addParameter("location","Near The Lake")
+        .addParameter("doing","Feeding on grass")
+        .addParameter("animal","zebra")
+        .addParameter("date","21st")
+        .addParameter("month","September")
+        .executeUpdate();
+    }
 
 
 }
